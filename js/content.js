669e103c58efc48da50feb22d4f93fc246ce1294
dedcha <brahmadedcha@gmail.com>
2022-07-20 21:19:38 +0530

    pagination added

--- js/content.js
+++ js/content.js
@@ -9,19 +9,19 @@ let articleList = [
     {src: "assets/skip.png", article: "first"},
     {src: "assets/slide.png", article: "second"},
     {src: "assets/skip.png", article: "first"},
-    {src: "assets/slide.png", article: "second"},
+    {src: "assets/slide.png", article: "3"},
     {src: "assets/skip.png", article: "ParagraphParagraphsarethegroupofsentencescombinedtogether,aboutacertaintopic.Itisaveryimportantformofwritingaswewritealmosteverythinginparagraphs,beitananswer,essay,story,emails,etc.Wecansaythatawell-structuredparagraphistheessenceofgoodwriting.Thepurposesoftheparagrapharetogiveinformation,toexplainsomething,totellastory,andtoconvincesomeonethatourideaisright.Paragraphsareblocksoftextualcontentthatsegmentoutalargerpieceofwriting—stories,novels,articles,creativewriting,orprofessionalwritingportions—makingitlesscomplicatedtoreadandunderstand.Excellentparagraphsareanavailablewritingskillforplentyoftypesofliterature,andproperwriterscansubstantiallybeautifytheclarityoftheirnews,essays,orfictionwritingwhilstconstructingnicely."},
     {src: "", article: "second"},
-    {src: "assets/skip.png", article: "first"},
-    {src: "assets/slide.png", article: "second"},
-    {src: "assets/skip.png", article: "first"},
-    {src: "assets/slide.png", article: "second"},
+    {src: "assets/skip.png", article: "4"},
+    {src: "assets/slide.png", article: "5"},
+    {src: "assets/skip.png", article: "6"},
+    {src: "assets/slide.png", article: "7"},
     {src: "assets/skip.png", article: "ParagraphParagraphsarethegroupofsentencescombinedtogether,aboutacertaintopic.Itisaveryimportantformofwritingaswewritealmosteverythinginparagraphs,beitananswer,essay,story,emails,etc.Wecansaythatawell-structuredparagraphistheessenceofgoodwriting.Thepurposesoftheparagrapharetogiveinformation,toexplainsomething,totellastory,andtoconvincesomeonethatourideaisright.Paragraphsareblocksoftextualcontentthatsegmentoutalargerpieceofwriting—stories,novels,articles,creativewriting,orprofessionalwritingportions—makingitlesscomplicatedtoreadandunderstand.Excellentparagraphsareanavailablewritingskillforplentyoftypesofliterature,andproperwriterscansubstantiallybeautifytheclarityoftheirnews,essays,orfictionwritingwhilstconstructingnicely."},
-    {src: "", article: "second"},
-    {src: "assets/skip.png", article: "first"},
-    {src: "assets/slide.png", article: "second"},
-    {src: "assets/skip.png", article: "first"},
-    {src: "assets/slide.png", article: "second"},
+    {src: "", article: "8"},
+    {src: "assets/skip.png", article: "9"},
+    {src: "assets/slide.png", article: "10"},
+    {src: "assets/skip.png", article: "11"},
+    {src: "assets/slide.png", article: "12"},
 
 ];
 
@@ -47,31 +47,66 @@ const ContentItem = (props) => {
 
 
 const Content = (p) => {
-    const items = p.items;
+    const { useState, useEffect } = React;
+    const [items, setItems]=useState(p.items);
+    const [tempItems, setTempItems]=useState([]);
+    const [pageStart, setPageStart]=useState(0);
+
+    useEffect(() => {
+        var tItems=items.slice(0,5);
+        setTempItems(tItems);
+        console.log(tempItems, items);
+    }, []);
+
+    const [checkTempItems,setCheckTempItems] = useState(false);
+
+    useEffect(() => {
+        if(tempItems.length>0) setCheckTempItems(true);
+    }, [tempItems]);
+
+    const nextPage = () => {
+        if(items.length<5) return;
+        if(pageStart>=items.length-5) {
+            var tempList = items.slice(pageStart, items.length);
+            setTempItems(tempList);
+            return;
+        };
+        setPageStart(pageStart+5);
+        var tempList = items.slice(pageStart, pageStart+5);
+        setTempItems(tempList);
+    }
+
+    const prevPage = () => {
+        if(items.length<5) return;
+        if(pageStart<=0) {
+            var tempList = items.slice(pageStart, pageStart+items.length);
+            setTempItems(tempList);
+            return;
+        };
+        setPageStart(pageStart-5);
+        var tempList = items.slice(pageStart, pageStart+5);
+        setTempItems(tempList);
+    }
+
     return (
         <div className="content-list">
-            {items && items.map(item => (
+            {tempItems && tempItems.map(item => (
                 <ContentItem article={item.article} src={item.src} />
             ))}
 
-            <div className="paginator"><Paginator /></div>
+            {!checkTempItems && <div>Loading..</div>}
+
+            <div className="paginator"><Paginator nextPage={nextPage} prevPage={prevPage} /></div>
         </div>
     )
 }
 
-const nextPage = () => {
-    console.log("paggingggggg");
-}
 
-const prevPage = () => {
-    console.log("paggingggggg");
-}
-
-const Paginator = () => {
+const Paginator = ({nextPage, prevPage}) => {
     return (
         <div className="pagi-buttons">
-            <button onClick={nextPage}>Prev Page</button>
-            <button onClick={prevPage}>Next Page</button>
+            <button onClick={prevPage}>Prev Page</button>
+            <button onClick={nextPage}>Next Page</button>
         </div>
     );
 }
