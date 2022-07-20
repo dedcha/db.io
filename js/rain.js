0fa47a7837469f1045243a5e999b64924d8a4cba
dedcha <brahmadedcha@gmail.com>
2022-07-20 14:37:47 +0530

    rain added

--- js/rain.js
+++ js/rain.js
@@ -1,49 +1,60 @@
-
-let c = document.getElementById("rain-canvas");
-//initial
-var w = c.width = window.innerWidth,
-h = c.height = window.innerHeight,
-rctx = c.getContext('2d'),
-
-//parameters
-total = w,
-accelleration = .05,
-
-//afterinitial calculations
-size = w/total,
-occupation = w/total,
-repaintColor = 'rgba(0, 0, 0, .04)'
-colors = [],
-dots = [],
-dotsVel = [];
-
-//setting the colors' hue
-//and y level for all dots
-var portion = 360/total;
-for(var i = 0; i < total; ++i){
-  colors[i] = portion * i;
-  
-  dots[i] = h;
-  dotsVel[i] = 10;
-}
-
-function anim(){
-  window.requestAnimationFrame(anim);
-  
-  rctx.fillStyle = repaintColor;
-  rctx.fillRect(0, 0, w, h);
-  
-  for(var i = 0; i < total; ++i){
-    var currentY = dots[i] - 1;
-    dots[i] += dotsVel[i] += accelleration;
+document.addEventListener("DOMContentLoaded", function() {
+    var canvas = document.getElementById("rain-canvas");
+    canvas.height = document.documentElement.clientHeight;
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
     
-    rctx.fillStyle = 'hsl('+ colors[i] + ', 80%, 50%)';
-    rctx.fillRect(occupation * i, currentY, size, dotsVel[i] + 1);
-    
-    if(dots[i] > h && Math.random() < .01){
-      dots[i] = dotsVel[i] = 0;
+    if(canvas.getContext) {
+      var ctx = canvas.getContext('2d');
+      var w = canvas.width;
+      var h = canvas.height;
+      ctx.strokeStyle = 'rgba(174,194,224,0.5)';
+      ctx.lineWidth = 1;
+      ctx.lineCap = 'round';
+      
+      
+      var init = [];
+      var maxParts = 1000;
+      for(var a = 0; a < maxParts; a++) {
+        init.push({
+          x: Math.random() * w,
+          y: Math.random() * h,
+          l: Math.random() * 1,
+          xs: -4 + Math.random() * 4 + 2,
+          ys: Math.random() * 10 + 10
+        })
+      }
+      
+      var particles = [];
+      for(var b = 0; b < maxParts; b++) {
+        particles[b] = init[b];
+      }
+      
+      function draw() {
+        ctx.clearRect(0, 0, w, h);
+        for(var c = 0; c < particles.length; c++) {
+          var p = particles[c];
+          ctx.beginPath();
+          ctx.moveTo(p.x, p.y);
+          ctx.lineTo(p.x + p.l * p.xs, p.y + p.l * p.ys);
+          ctx.stroke();
+        }
+        move();
+      }
+      
+      function move() {
+        for(var b = 0; b < particles.length; b++) {
+          var p = particles[b];
+          p.x += p.xs;
+          p.y += p.ys;
+          if(p.x > w || p.y > h) {
+            p.x = Math.random() * w;
+            p.y = -20;
+          }
+        }
+      }
+      
+      setInterval(draw, 30);
+      
     }
-  }
-}
-
-anim();
+  });
